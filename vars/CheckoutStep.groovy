ceca527a38d0422c29d5ab2dd6e8a786f72a5477
galihanggara68 <galihanggara68@gmail.com>
2022-11-18 13:48:47 +0700

    + Added workspaceName to step

--- vars/CheckoutStep.groovy
+++ vars/CheckoutStep.groovy
@@ -6,7 +6,7 @@ def call(Map config = [:]){
         if(config.branchName){
             checkout([$class: 'GitSCM', branches: [[name: config.branchName]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: config.credentialsId, url: config.url]]])
         }else{
-            checkout([$class: 'TeamFoundationServerScm', projectPath: config.projectPath, serverUrl: config.serverUrl, useOverwrite: true, useUpdate: true, userName: "$USER", password: hudson.util.Secret.fromString("$PASS"), workspaceName: 'Hudson-${JOB_NAME}-${NODE_NAME}-CLIENT'])
+            checkout([$class: 'TeamFoundationServerScm', projectPath: config.projectPath, serverUrl: config.serverUrl, useOverwrite: true, useUpdate: true, userName: "$USER", password: hudson.util.Secret.fromString("$PASS"), workspaceName: config.workspaceName ? config.workspaceName : 'Hudson-${JOB_NAME}-${NODE_NAME}-CLIENT'])
         }
     }
     if(!isUnix()){
