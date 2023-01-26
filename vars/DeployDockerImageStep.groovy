6a66a4a60d1678a3ae31e8d18a649b4805486239
galihanggara68 <galihanggara68@gmail.com>
2023-01-26 17:27:57 +0700

    + Added AWS CLI cloud type

--- vars/DeployDockerImageStep.groovy
+++ vars/DeployDockerImageStep.groovy
@@ -19,6 +19,18 @@ def call(Map config = [:]){
         dockerImageRemote = docker.build "${config.imageName}:build-${env.BUILD_ID}"
         dockerImageRemote.push()
         dockerImageRemote.push("cloud")
+    }else if(config.cloudType == "AWS CLI"){
+        withCredentials([usernamePassword(credentialsId: "${config.credentialsId}", passwordVariable: 'SECRET', usernameVariable: 'KEY')]) {
+            sh "echo [default] > ~/.aws/credentials"
+            sh "echo aws_secret_access_key = $KEY >> ~/.aws/credentials"
+            sh "echo aws_secret_access_secret = $SECRET >> ~/.aws/credentials"
+            sh "echo region = ${config.regionId} >> ~/.aws/credentials"
+            sh "aws ecr get-login-password > ~/aws_creds.txt"
+            sh "cat ~/aws_creds.txt | docker login --username AWS --password-stdin ${config.registryURL}"
+        }
+        dockerImageRemote = docker.build "${config.imageName}:build-${env.BUILD_ID}"
+        dockerImageRemote.push()
+        dockerImageRemote.push("cloud")
     }else if(config.cloudType == "Azure"){
         echo "Azure Provider is under maintenance or unavailable"
     }
