1ef403ff1f23c4e97b0259b37628df4dda71d9e2
galihanggara68 <galihanggara68@gmail.com>
2023-02-02 09:12:15 +0700

    + Added DeployWorkload at AWS CLI cloudType

--- vars/DeployWorkloadStep.groovy
+++ vars/DeployWorkloadStep.groovy
@@ -2,30 +2,32 @@ def call(Map config = [:]){
     if(config.cloudType.notBlank){
         if(config.cloudType == "Google Cloud"){
             withCredentials([file(credentialsId: "${config.credentialsId}", variable: 'FILE')]) {
-                bat "gcloud auth activate-service-account ${config.serviceAccountName} --key-file=%FILE%"
-                bat "gcloud container clusters get-credentials ${config.clusterName} --zone ${config.zone}"
+                sh "gcloud auth activate-service-account ${config.serviceAccountName} --key-file=%FILE%"
+                sh "gcloud container clusters get-credentials ${config.clusterName} --zone ${config.zone}"
             }
         }else if(config.cloudType == "AWS CLI"){
+            if(!env.AWS_DEFAULT_REGION){
+                env.AWS_DEFAULT_REGION = config.regionId
+            }
             withCredentials([usernamePassword(credentialsId: "${config.credentialsId}", passwordVariable: 'SECRET', usernameVariable: 'KEY')]) {
                 sh "aws configure set aws_access_key_id $KEY"
                 sh "aws configure set aws_secret_access_key $SECRET"
-                sh "aws ecr get-login-password > ~/aws_creds.txt"
-                sh "cat ~/aws_creds.txt | docker login --username AWS --password-stdin ${config.registryURL}"
+                sh "aws eks update-kubeconfig --name ${config.clusterName}"
             }
         }
 
-        bat "kubectl apply -f namespace.yaml"
+        sh "kubectl apply -f namespace.yaml"
 
-        bat "kubectl apply -f deployment.yaml"
-        bat "kubectl apply -f service.yaml"
+        sh "kubectl apply -f deployment.yaml"
+        sh "kubectl apply -f service.yaml"
 
         if(config.resetConfigmap){
-            bat "kubectl apply -f configmap.yaml"
+            sh "kubectl apply -f configmap.yaml"
         }
 
         if(config.autoRestart){
             def deployment = readYaml(file: "deployment.yaml")
-            bat "kubectl rollout restart deployments/${deployment.metadata.labels.app} -n ${deployment.metadata.namespace}"
+            sh "kubectl rollout restart deployments/${deployment.metadata.labels.app} -n ${deployment.metadata.namespace}"
         }
     }
     
